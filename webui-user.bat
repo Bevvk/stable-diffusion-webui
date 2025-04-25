c9a8656cf79758d01ea32373b99892221d9818b0
Bevvk <124668512+Bevvk@users.noreply.github.com>
2025-04-26 00:35:31 +0800

    update

--- webui-user.bat
+++ webui-user.bat
@@ -1,6 +1,8 @@
 @echo off
-call conda activate sd-webui
-python -c "import torch; print('PyTorch version:', torch.__version__)"
-set COMMANDLINE_ARGS=--lowvram --xformers
-set PIP_IGNORE_INSTALLED=1
+
+set PYTHON=
+set GIT=
+set VENV_DIR=
+set COMMANDLINE_ARGS=
+
 call webui.bat
